3b5a0bd66713ab6fb373566efb2e024b38fdcc81
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-02-14 21:07:13 +0530

    change the winning score to 100 in app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         document.getElementById('score-' + activePlayer).textContent = scores[activePlayer]; // best approach
 
         // Check if player won the game
-        if(scores[activePlayer] >= 20) {
+        if(scores[activePlayer] >= 100) {
             document.getElementById('name-' + activePlayer).textContent = 'Winner!!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
